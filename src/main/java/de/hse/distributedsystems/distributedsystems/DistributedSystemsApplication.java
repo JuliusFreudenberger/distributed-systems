f40333cbf1773557029ceef2a5e82f3cbb434d81
Julius Freudenberger <julius+git@jfreudenberger.de>
2022-04-03 07:13:30 +0200

    Add intial spring template

---
+++ src/main/java/de/hse/distributedsystems/distributedsystems/DistributedSystemsApplication.java
@@ -0,0 +1,13 @@
+package de.hse.distributedsystems.distributedsystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSystemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedSystemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/hse/distributedsystems/distributedsystems/DistributedSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hse.distributedsystems.distributedsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedSystemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
