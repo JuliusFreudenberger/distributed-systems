46e13a07f46bab74f7b90c25e8caeef3072b5f94
Julius Freudenberger <julius+git@jfreudenberger.de>
2022-04-10 23:17:38 +0200

    Add person model

---
+++ src/main/java/de/hse/distributedsystems/distributedsystems/model/Person.java
@@ -0,0 +1,15 @@
+package de.hse.distributedsystems.distributedsystems.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class Person {
+
+    private String name;
+    private int age;
+
+}
